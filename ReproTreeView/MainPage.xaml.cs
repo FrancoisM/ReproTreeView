52bd71afe6ad9e2d669a3300d6e8f652c6bf81b4
Francois <francois.mouchati@gmail.com>
2019-06-03 16:58:51 +0200

    Repro

---
+++ ReproTreeView/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace ReproTreeView
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = new MainPageViewModel();
+        }
+    }
+
+    public class MainPageViewModel : ReactiveObject
+    {
+        private List<Item> _items;
+        public List<Item> Items
+        {
+            get => _items;
+            private set => this.RaiseAndSetIfChanged(ref _items, value);
+        }
+
+        public MainPageViewModel()
+        {
+            //Items = GetDessertData();
+            GetDesertDataSync();
+        }
+
+        private async Task GetDesertDataSync()
+        {
+            await Task.Delay(2000);
+            Items = GetDessertData();
+        }
+
+        private List<Item> GetDessertData() => new List<Item>
+            { 
+                new Item("Flavors", new List<Item>
+                {
+                    new Item("Vanilla", null),
+                    new Item("Strawberry", null),
+                    new Item("Chocolate", null)
+                }),
+                new Item("Toppings", new List<Item>
+                {
+                    new Item("Candy", new List<Item>
+                    {
+                        new Item("Chocolate", null),
+                        new Item("Mint", null),
+                        new Item("Sprinkles", null)
+                    }),
+                    new Item("Fruits", new List<Item>
+                    {
+                        new Item("Mango", null),
+                        new Item("Peach", null),
+                        new Item("Kiwi", null)
+                    }),
+                    new Item("Berries", new List<Item>
+                    {
+                        new Item("Strawberry", null),
+                        new Item("Blueberry", null),
+                        new Item("Blackberry",null)
+                    })
+                })
+            };
+    }
+
+    public class Item
+    {
+        public string Name { get; }
+        public List<Item> Children { get; }
+        public Item(string name, List<Item> children)
+        {
+            Name = name;
+            Children = children;
+        }
+    }
+}
